e1e76aef1dff1778982895b6ac95915c9b60b7fd
Andres Fernando <andres.fernando08@gmail.com>
2020-04-09 16:17:26 -0500

    enlazar css y js en plantilla

--- footer.php
+++ footer.php
@@ -3,5 +3,6 @@
         This is the footer area
     </footer>
 
+    <?php wp_footer(); ?>
     </body>
 </html>
--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function awesome_scripts_enqueue() {
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'awesome_scripts_enqueue');
