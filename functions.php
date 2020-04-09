ead51b60e9ac51fdf7bf96378f0aa8c4278add05
Andres Fernando <andres.fernando08@gmail.com>
2020-04-09 16:12:30 -0500

    incluir archivos de css y js custom

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function awesome_scripts_enqueue() {
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'awesome_scripts_enqueue');
