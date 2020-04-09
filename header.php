33bc7acc3e33b712f24488c8d25df70920c8d1a3
Andres Fernando <andres.fernando08@gmail.com>
2020-04-09 17:09:53 -0500

    clases dinamicas en el body

--- header.php
+++ header.php
@@ -6,5 +6,15 @@
         <?php wp_head(); ?>
     </head>
 
-    <body>
+    <?php 
+        // is_front_page: it is the main page used as static in the settings
+        // is_home: it is the blog loop page
+        if ( is_front_page() ): 
+            $awesome_classes = ['awesome-class', 'my-class'];
+        else: 
+            $awesome_classes = ['no-awesome-class'];
+        endif;
+    ?>
+
+    <body <?php body_class( $awesome_classes ); ?>>
         <?php wp_nav_menu(['theme_location' => 'primary']); ?>
